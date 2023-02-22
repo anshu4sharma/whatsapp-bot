0303d92b716449b0b2727533c6f7cb233a43f70c
anshu0x <anshusharma6327@gmail.com>
2023-02-22 14:19:04 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
-    authStrategy: new LocalAuth(), puppeteer: {
+    puppeteer: {
         args: ['--no-sandbox', "--disable-setuid-sandbox"]
     },
 });
